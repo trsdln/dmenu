d21b4fddb21ce516acb29c4ed98ded18a0757e8e
Taras <trsdln@gmail.com>
2020-07-04 00:55:08 +0300

    Reconfigure fonts

--- config.h
+++ config.h
@@ -5,7 +5,8 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Hack:style=bold:antialias=true:size=10"
+  "Hack:style=bold:antialias=true:size=10",
+  "Noto Color Emoji:style=bold:antialias=true:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
