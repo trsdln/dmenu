54979aca09fb4e21881ebddd83b70665ddee9d3a
Taras <trsdln@gmail.com>
2021-05-31 23:48:49 +0300

    Adjust for wal integration

---
+++ colors-wal-dmenu.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+  /*     fg         bg       */
+  [SchemeNorm] = { "#7e838d", "#282c34" },
+  [SchemeSel] = { "#d6d6d6", "#282c34" },
+  [SchemeOut] = { "#000000", "#00ffff" },
+};
--- config.h
+++ config.h
@@ -1,5 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
+#include "colors-wal-dmenu.h"
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
@@ -10,12 +11,6 @@ static const char *fonts[] = {
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#7e838d", "#282c34" },
-	[SchemeSel] = { "#d6d6d6", "#282c34" },
-	[SchemeOut] = { "#000000", "#00ffff" },
-};
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 
